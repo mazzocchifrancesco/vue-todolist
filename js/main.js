606071e8e209c758531ea710cf93eef8db5013cc
FrancescoMazzocchi <123@email.it>
2023-11-18 09:26:16 +0100

    feat: print list, ternary class

--- js/main.js
+++ js/main.js
@@ -3,9 +3,24 @@ const { createApp } = Vue;
 const object = {
     data: function () {
         return {
+            toDoList: [
+                {
+                    testo: 'fare la spesa',
+                    done: true
+                },
+                {
+                    testo: 'aggiustare pollaio',
+                    done: false
+                },
+                {
+                    testo: 'portare fuori il cane',
+                    done: true
+                }
+            ],
 
         }
     },
 };
 
 createApp(object).mount('#app')
+
